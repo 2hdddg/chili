9653be179bfd53e59dde3687f0ea145571670500
Peter Wilhelmsson <2hdddg@gmail.com>
2016-06-15 20:50:20 +0200

    Printing and failing in example

--- examples/lots_of_printfs.c
+++ examples/lots_of_printfs.c
@@ -5,4 +5,15 @@ int test_print_a_lot()
     for (i = 0; i < 25; i++){
         printf("Printing %d\n", i);
     }
+    return 1;
+}
+
+int test_print_a_lot_and_fail()
+{
+    int i;
+
+    for (i = 0; i < 25; i++){
+        printf("Printing and failing %d\n", i);
+    }
+    return 0;
 }
