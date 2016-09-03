c88ba38fd0e7723978500fd055ddd2b7116ee7bf
Peter Wilhelmsson <2hdddg@gmail.com>
2016-09-03 22:55:51 +0200

    Doxygen

--- src/redirect.h
+++ src/redirect.h
@@ -3,42 +3,50 @@
 #define CHILI_REDIRECT_MAX_PATH 255
 #define CHILI_REDIRECT_MAX_NAME 255
 
-/* Initializes redirection.
- * enable - flag to enable/disable, checked by other
- *          functions in module.
- * path   - path to directory where redirection files
- *          will be placed, created if it doesnt exist
+/* @brief Initializes redirection.
+ * @param enable Flag to enable/disable, checked by other
+ *        functions in module.
+ * @param path Directory where redirection files
+ *        will be placed, created if it doesnt exist
  *
- * Returns neg on error, pos on success.
+ * @return Negative on error, positive on success.
+ * @return Void
 */
 int chili_redirect_begin(int enable, const char *path);
 
-/* Redirects stdout to a file. If module is disabled
- * this will do nothing.
+/* @bref Redirects stdout to a file.
  *
- * name - name of file that will contain stdout
+ * If module is disabled this will do nothing.
+ *
+ * @param name Name of file that will contain stdout
+ * @return Void
 */
 void chili_redirect_start(const char *name);
 
-/* Restores stdout to it's former glory.
+/* @brief Restores stdout to it's former glory.
+ * @return Void
 */
 void chili_redirect_stop();
 
-/* Prints a previously redirected stdout session
- * to stdout. If module is disabled this will print
- * nothing.
+/* @brief Prints a previously redirected stdout session
+ *        to stdout.
+ *
+ * If module is disabled this will print nothing.
  * Redirection needs to be stopped prior to calling
  * this function.
  *
- * name - name used to start redirection
- * before - string to send to stdout before content,
- *          or null if nothing to print
- * after - string to send to stdout after content,
-           or null if nothing to print
+ * @param name Name used to start redirection
+ * @param before String to send to stdout before content,
+ *        or null if nothing to print
+ * @param after String to send to stdout after content,
+          or null if nothing to print
+ * @return Void
 */
 void chili_redirect_print(const char *name, const char *before,
                           const char *after);
 
-/* Releases allocated resources
+/* @brief Releases allocated resources
+ * @return Void
 */
 void chili_redirect_end();
+
--- src/suite.h
+++ src/suite.h
@@ -9,37 +9,41 @@ struct chili_suite {
     int count;
 };
 
-/* Starts building a suite of tests.
+/* @brief Starts building a suite of tests.
  *
- * max_count - number of symbols that will be
- *             evaluated
+ * @param max_count Number of symbols that will be
+ *        evaluated.
  *
- * Returns neg on error, pos on success.
+ * @return Negative on error, positive on success.
 */
 int chili_suite_begin(int max_count);
 
-/* Evaluates a symbol. Adds symbol to suite
- * if it matches. Call once for each symbol.
+/* @brief Evaluates a symbol.
+ *
+ * Adds symbol to suite if it matches. Call once for each symbol.
  * Can only be called after chili_suite_begin.
  *
- * symbol - symbol name
+ * @param symbol - symbol name
  *
  * Returns neg on error, pos on success.
 */
 int chili_suite_eval(char *symbol);
 
-/* Retrieves the suite. Should be called after
- * all symbols have been evaluated.
+/* @brief Retrieves the suite. 
  *
- * suite - set to result of evaluation, pointer
- *         is valid until call to chili_suite_end
+ * Should be called after all symbols have been evaluated.
+ *
+ * @param suite Set to result of evaluation, pointer
+ *        is valid until call to chili_suite_end
  *
  * Returns neg on error, pos on success.
 */
 int chili_suite_get(struct chili_suite **suite);
 
-/* Frees allocated resources.
+/* @brief Frees allocated resources.
+ *
  * Pointer to suite is invalid after calling
  * this
 */
 void chili_suite_end();
+
